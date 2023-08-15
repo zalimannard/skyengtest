6c9c1513e91873daaf1bf0a7d82c89b9088c7e36
Dmitry Kolesnikov <kolesnikov.dmitry.mail@gmail.com>
2023-08-15 21:36:38 +0300

    init: Пустой проект на Spring с зависимостями

---
+++ src/main/java/ru/kolesnikovdmitry/skyengtest/SkyengtestApplication.java
@@ -0,0 +1,13 @@
+package ru.kolesnikovdmitry.skyengtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyengtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyengtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kolesnikovdmitry/skyengtest/SkyengtestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kolesnikovdmitry.skyengtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyengtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
